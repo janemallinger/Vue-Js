a916980c204a14e95dd6ebf0284a4fbf63cbc91c
Jane Allinger <janeallinger@Janes-Air.lan>
2025-03-09 20:35:18 -0500

    Pass data to a parent component with custom events

---
+++ src/BlogPost.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="blog-post">
+        <p>
+            {{ message }}
+        </p>
+
+        <h2>{{ id }} - {{ blogPostTitle }}</h2>
+        <h4>{{ blogPostContent }}</h4>
+        <button @click="$emit('delete-blog-post', id)">Delete Post</button>
+        <button @click="emitDeletePostEvent(id)">Delete Post</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+let message = ref('This is is the BlogPost component.')
+
+defineProps(['id','blogPostTitle','blogPostContent'])
+
+ const emit = defineEmits(['delete-blog-post'])
+
+function emitDeletePostEvent(id) {
+    emit('delete-blog-post', id)
+}
+
+</script>
+
+<style scoped>
+
+.blog-post{
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+</style>
