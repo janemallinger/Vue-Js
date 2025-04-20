f2309a08e296cc0a89436593acb17c6340b13bc0
Jane Allinger <janeallinger@Janes-MacBook-Air.local>
2025-04-20 00:02:41 -0500

    Named views.

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import BlogPosts from '@/views/BlogPosts.vue'
 import About from '@/views/About.vue'
 import BlogPost from '@/views/BlogPost.vue'
 import BlogPostsGreeting from '@/views/BlogPostsGreeting.vue'
+import Ads from '@/views/Ads.vue'
 
 // Create a router instance
 const router = createRouter({
@@ -21,7 +22,10 @@ const router = createRouter({
       redirect: { name: 'blogPostsGreeting' },
       children: [
         { path: '', name: 'blogPostsGreeting', component: BlogPostsGreeting },
-        { path: '/blogPosts/:id(\\d+)', name: 'blogPost', component: BlogPost }
+        { path: '/blogPosts/:id(\\d+)', name: 'blogPost', components: {
+          default: BlogPost,
+          sidebar: Ads,
+        } }
       ], 
     },
     { path: '/about', name: 'about', component: About },
