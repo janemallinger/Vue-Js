91c3cc0cd5f5285b097b105407f4063971e777ea
Jane Allinger <janeallinger@Janes-Air.lan>
2025-04-21 20:09:42 -0500

    Scroll behavior.

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,18 @@ import { isAuthenticated } from '@/apis/auth'
 const router = createRouter({
   // Provide the history implementation to use. We are using the HTML5 history API
   history: createWebHistory(),
+  scrollBehavior(to, from, savedPosition) {
+    const scrollBehaviorOptions = {
+      top: 0,
+      behavior: 'smooth',
+    }
+
+    if (to.meta.scrollToElement) {
+      scrollBehaviorOptions.el = to.meta.scrollToElement
+    }
+
+    return savedPosition ?? scrollBehaviorOptions
+  },
   // Define some routes, each route record should map to a component
   routes: [
     {
@@ -40,7 +52,10 @@ const router = createRouter({
               default: BlogPost,
               sidebar: Ads,
               }, 
-              meta: { requiresAuth: false }
+              meta: { 
+                requiresAuth: true, 
+                scrollToElement: '.blog-posts-layout' 
+              },
             },
           ], 
         },
